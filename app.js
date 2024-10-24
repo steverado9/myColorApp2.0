7f3e3153efb6b92bdc65a6b4ce4324de818e990d
stephen <isaacsteverado9@gmail.com>
2024-10-24 19:56:27 +0100

    fixed app.js

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const generateNumber = document.forms["generate-numbers"]; //getting the form
-//function to remove children of parentElement Div
-function removesChildern() {
-    const div = document.querySelector('#container') //get the div element
-    const childDivs = document.querySelectorAll('#container div'); // i got the children of the parent div element
-    Array.from(childDivs).forEach(function(childDiv){ //converted the node list to an array
-        if(childDiv.className = "child-div") { //check the className using if statement
-            div.removeChild(childDiv) //delete child
-        }
-    })
-}
+const parentDiv = document.querySelector("#container"); //getting the parent div element
+
 //Adding event to the form
 generateNumber.addEventListener('submit', function (e) { //adding a submit event
     e.preventDefault(); //prevent the form from refreshing when i click submit
-    value = generateNumber.querySelector('input').value; //getting the number typed
-    removesChildern();
+    const value = generateNumber.querySelector('input').value; //getting the number typed
+    parentDiv.innerHTML = ""; //clearing the div by maiking it empty
     for (let number = 1; number <= value; number++) { //generating numbers from one to whatever value typed
-        const parentDiv = document.querySelector("#container"); //getting the parent div element
         //create, add class and append element
         const div = document.createElement('div'); //create div element
         div.textContent = number; //added content
