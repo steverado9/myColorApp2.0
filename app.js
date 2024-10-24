3342868bbb489aafa94beda4cbe2f96b531d8e67
stephen <isaacsteverado9@gmail.com>
2024-10-24 18:20:42 +0100

    fixed the search bar

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const generateNumber = document.forms["generate-numbers"]; //getting the form
-
+//function to remove children of parentElement Div
+function removesChildern() {
+    const div = document.querySelector('#container') //get the div element
+    const childDivs = document.querySelectorAll('#container div'); // i got the children of the parent div element
+    Array.from(childDivs).forEach(function(childDiv){ //converted the node list to an array
+        if(childDiv.className = "child-div") { //check the className using if statement
+            div.removeChild(childDiv) //delete child
+        }
+    })
+}
+//Adding event to the form
 generateNumber.addEventListener('submit', function (e) { //adding a submit event
     e.preventDefault(); //prevent the form from refreshing when i click submit
     value = generateNumber.querySelector('input').value; //getting the number typed
+    removesChildern();
     for (let number = 1; number <= value; number++) { //generating numbers from one to whatever value typed
         const parentDiv = document.querySelector("#container"); //getting the parent div element
         //create, add class and append element
@@ -18,7 +29,6 @@ generateNumber.addEventListener('submit', function (e) { //adding a submit event
         } else {
             div.style.backgroundColor = "yellow";
         }
-
 
         //prime number check
         const primeNumber = isPrimeNumber(number);
@@ -47,16 +57,8 @@ function isPrimeNumber(n) {
     return true;
 }
 
-//function to clear the input
-const div = document.querySelector('#container') //get the div element
-generateNumber.addEventListener('keyup', function(e){ //i added an event to the form
-    const childDivs = document.querySelectorAll('#container div'); // i got the children of the parent div element
-    Array.from(childDivs).forEach(function(childDiv){ //converted the node list to an array
-        if(childDiv.className = "child-div") { //check the className using if statement
-            div.removeChild(childDiv) //delete child
-        }
-    })
-})
+
+
 
 
     
